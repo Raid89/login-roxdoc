075b0ac7bcfc649316424b5199d53c66ea8c878d
Raid89 <nicolas.cr0603@gmail.com>
2024-04-24 20:53:28 -0500

    Se realiza el diseño del Login según el diseño en figma

---
+++ src/js/main.js
@@ -0,0 +1,26 @@
+// Función para el toggle de la contraseña
+const buttonTooglePass = document.getElementById('togglePasswordButton');
+
+const tooglePassword = () => {
+    const typeInputPassword = document.getElementById('inputPassword')?.type;
+    const newInputType = typeInputPassword === 'text' ? 'password' : 'text';
+    inputPassword.type = newInputType
+}
+
+buttonTooglePass.addEventListener('click', tooglePassword);
+
+
+// Función para guardar los datos del formulario y mostrarlos en consola
+const formButtonLogin = document.getElementById('formButtonLogin');
+
+const guardarDatosFormulario = (event) => {
+    event.preventDefault();
+    const inputUsuario = document.getElementById('inputUsuario').value;
+    const inputPassword = document.getElementById('inputPassword').value;
+
+    console.log('Datos del formulario:');
+    console.log('Usuario:', inputUsuario);
+    console.log('Contraseña:', inputPassword);
+}
+
+formButtonLogin.addEventListener('click', guardarDatosFormulario);
